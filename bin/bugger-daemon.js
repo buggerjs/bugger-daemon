0128c5ae23c657d6e0dc7f788f13dd74bbbae917
Jan Krems <jan.krems@groupon.com>
2013-07-28 19:14:29 +0200

    Even more awesome interface

--- bin/bugger-daemon.js
+++ bin/bugger-daemon.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var createBuggerServer = require('../lib/bugger-daemon.js');
-createBuggerServer()
+createBuggerServer().httpServer
 .listen(8058, '127.0.0.1', function() {
   var address = this.address();
   console.log('bugger-daemon listening:', address);
--- test/helpers/test_server.js
+++ test/helpers/test_server.js
@@ -62,7 +62,7 @@ module.exports = function getTestServer() {
 
   var server;
   beforeEach(function(done) {
-    server = buggerd()
+    server = buggerd().httpServer
     .listen(8050, '127.0.0.1', function() {
       return done();
     });
